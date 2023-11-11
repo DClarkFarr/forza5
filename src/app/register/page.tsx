a2b624de8042234a233017ace99fbf91c1a5922e
Daniel Farr <daniel@aptivada.com>
2023-11-11 15:08:28 -0700

    prisma working

---
+++ src/app/register/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export function metadata(): Metadata {
+    return {
+        title: "Register - Forza5 Calculator",
+        description: "Your first step is here",
+    };
+}
+
+export default function RegisterPage() {
+    return (
+        <div className="register-page">
+            <h1>Create account here!</h1>
+        </div>
+    );
+}
