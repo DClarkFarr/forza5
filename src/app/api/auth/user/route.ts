634bb8ae29a4ef6fd6152f853956486b7f613797
Daniel Farr <daniel@aptivada.com>
2023-11-11 20:04:55 -0700

    get iron session working

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -1,50 +1,46 @@
 import { registerUser } from "@/prisma/methods/user";
 import { NextResponse } from "next/server";
-import { withIronSessionApiRoute } from "iron-session/next";
-import { sessionOptions } from "@/methods/session";
-import { NextApiRequest, NextApiResponse } from "next";
-import toJSON from "@/methods/router/toJSON";
+import { ironSessionMiddleware } from "@/methods/session";
 
-export const POST = withIronSessionApiRoute(async (req, res) => {
+export const POST = ironSessionMiddleware(async (req, res) => {
     try {
-        const { name, email, password } = await toJSON(req.body);
+        const { name, email, password } = await req.json();
 
         const user = await registerUser(name, email, password);
 
         req.session.user = user;
         await req.session.save();
 
-        return res.json(user);
+        return NextResponse.json(user);
     } catch (err) {
         if (err instanceof Error) {
-            console.log("error was", err.message);
-            return res.status(403).json({ message: err.message });
+            return NextResponse.json({ message: err.message }, { status: 402 });
         } else {
-            return res.status(500).json({ message: "Unknown error" });
+            return NextResponse.json(
+                { message: "Unknown error" },
+                { status: 500 }
+            );
         }
     }
-}, sessionOptions);
+});
 
-export const GET = withIronSessionApiRoute(
-    async (req: NextApiRequest, res: NextApiResponse) => {
-        try {
-            if (!req.session.user) {
-                res.status(401).json({ message: "No session" });
-            }
-            return res.json(req.session.user);
-        } catch (err) {
-            if (err instanceof Error) {
-                return NextResponse.json(
-                    { message: err.message },
-                    { status: 402 }
-                );
-            } else {
-                return NextResponse.json(
-                    { message: "Unknown error" },
-                    { status: 500 }
-                );
-            }
+export const GET = ironSessionMiddleware(async (req, res) => {
+    try {
+        if (!req.session.user) {
+            return NextResponse.json(
+                { message: "No session" },
+                { status: 401 }
+            );
         }
-    },
-    sessionOptions
-);
+        return NextResponse.json(req.session.user);
+    } catch (err) {
+        if (err instanceof Error) {
+            return NextResponse.json({ message: err.message }, { status: 402 });
+        } else {
+            return NextResponse.json(
+                { message: "Unknown error" },
+                { status: 500 }
+            );
+        }
+    }
+});
--- src/methods/session.ts
+++ src/methods/session.ts
@@ -1,10 +1,11 @@
 // this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
-import type { IronSessionOptions } from "iron-session";
-import type { User } from "@/types/User";
+import { User } from "@/types/User";
+import { getIronSession, IronSession, IronSessionOptions } from "iron-session";
 
 export const sessionOptions: IronSessionOptions = {
     password: process.env.SECRET_COOKIE_PASSWORD as string,
     cookieName: "session_id",
+    // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
     cookieOptions: {
         secure: process.env.NODE_ENV === "production",
     },
@@ -16,3 +17,40 @@ declare module "iron-session" {
         user?: User;
     }
 }
+
+export type DynamicSegments = {
+    params: { slug: string } | undefined;
+};
+
+export type RouteHandler = (
+    request: Request,
+    routeSegment: DynamicSegments
+) => Promise<Response>;
+
+export type RouteHandlerWithSession = (
+    request: Request & { session: IronSession },
+    routeSegment: DynamicSegments
+) => Promise<Response>;
+
+export const ironSessionMiddleware = (
+    handler: RouteHandlerWithSession
+): RouteHandler => {
+    return async (request, routeSegment) => {
+        const cookieResponse = new Response();
+        const session = await getIronSession(
+            request,
+            cookieResponse,
+            sessionOptions
+        );
+
+        const sessionRequest = Object.assign(request, { session });
+        const response = await handler(sessionRequest, routeSegment);
+
+        const setCookie = cookieResponse.headers.get("set-cookie");
+        if (setCookie) {
+            response.headers.set("set-cookie", setCookie);
+        }
+
+        return response;
+    };
+};
