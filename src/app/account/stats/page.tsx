7afc187cfaf918804a7c0ea3136c4516e6ddbeaa
Daniel Farr <daniel@aptivada.com>
2023-11-17 16:03:55 -0700

    need to build client stats page

--- src/app/account/stats/page.tsx
+++ src/app/account/stats/page.tsx
@@ -1,7 +1,24 @@
+import { Metadata } from "next";
+
+export function metadata(): Metadata {
+    return {
+        title: "My Stats - Forza5 Calculator",
+        description:
+            "So much stuff going on here....add your stats dude, ita's not hard.",
+    };
+}
+
 export default async function MyStatsPage() {
     return (
         <div>
-            <h1>My Stats</h1>
+            <div className="flex">
+                <div className="flex-1">
+                    <h1>My Stats</h1>
+                </div>
+                <div className="flex-1">
+                    <button className="btn bg-sky-700">Add Stat</button>
+                </div>
+            </div>
         </div>
     );
 }
